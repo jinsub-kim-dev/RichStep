42a74cae661cef0c5624a941d0a0b758862b6ec0
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2025-05-06 23:24:00 +0900

    first commit

---
+++ src/main/kotlin/com/erving/richstep/RichstepApplication.kt
@@ -0,0 +1,11 @@
+package com.erving.richstep
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RichstepApplication
+
+fun main(args: Array<String>) {
+	runApplication<RichstepApplication>(*args)
+}
---
+++ src/test/kotlin/com/erving/richstep/RichstepApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.erving.richstep
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RichstepApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
