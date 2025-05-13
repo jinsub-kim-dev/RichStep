a064c733347c416563da1bf1b0bcf5c5e68d5c1d
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2025-05-13 21:01:38 +0900

    UserReadService 테스트 코드 추가

---
+++ src/test/kotlin/com/erving/richstep/user/service/UserServiceTest.kt
@@ -0,0 +1,59 @@
+package com.erving.richstep.user.service
+
+import com.erving.richstep.user.domain.User
+import com.erving.richstep.user.exception.UserNotFoundException
+import com.erving.richstep.user.service.port.FakeUserRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.BeforeEach
+import java.time.LocalDateTime
+import kotlin.test.Test
+
+class UserServiceTest {
+
+    private lateinit var userReadService: UserReadServiceImpl
+
+    @BeforeEach
+    fun init() {
+        val fakeUserRepository = FakeUserRepository()
+        this.userReadService = UserReadServiceImpl(fakeUserRepository)
+
+        fakeUserRepository.save(User(
+            id = 1,
+            email = "erving.kim@naver.com",
+            createdAt = LocalDateTime.now(),
+            modifiedAt = LocalDateTime.now()
+        ))
+
+        fakeUserRepository.save(User(
+            id = 2,
+            email = "erving.kim.dev@naver.com",
+            createdAt = LocalDateTime.now(),
+            modifiedAt = LocalDateTime.now()
+        ))
+    }
+
+    @Test
+    fun getById로_유저를_조회할_수_있다() {
+        // given
+        val targetUserId = 1L
+
+        // when
+        val findUser = this.userReadService.getById(targetUserId)
+
+        // then
+        assertThat(findUser.email == "erving.kim@naver.com")
+    }
+
+    @Test
+    fun 존재하지_않는_Id로_getById를_호출하면_에러를_던진다() {
+        // given
+        val targetUserId = 999L
+
+        // when
+        // then
+        assertThatThrownBy {
+            this.userReadService.getById(targetUserId)
+        }.isInstanceOf(UserNotFoundException::class.java)
+    }
+}
