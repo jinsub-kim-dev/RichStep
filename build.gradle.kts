a2f9335abd43f1d3085165b535819fbc6abb0bfa
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2025-05-17 22:34:21 +0900

    QueryDSL 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	id("org.springframework.boot") version "3.4.5"
 	id("io.spring.dependency-management") version "1.1.7"
 	kotlin("plugin.jpa") version "1.9.25"
+	kotlin("kapt") version "1.9.25"
 }
 
 group = "com.erving"
@@ -24,6 +25,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// QueryDSL (JPA + kapt)
+	implementation("com.querydsl:querydsl-jpa:5.0.0")
+	kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
@@ -40,6 +46,13 @@ allOpen {
 	annotation("jakarta.persistence.Entity")
 	annotation("jakarta.persistence.MappedSuperclass")
 	annotation("jakarta.persistence.Embeddable")
+}
+
+// QueryDSL Q파일 경로 설정
+sourceSets["main"].java.srcDirs("build/generated/querydsl")
+
+tasks.withType<JavaCompile>().configureEach {
+	options.generatedSourceOutputDirectory.set(file("build/generated/querydsl"))
 }
 
 tasks.withType<Test> {
