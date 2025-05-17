8866b7887290e20b61102098676e4df13781eacc
jinsub-kim-dev <jinsub.kim.dev@gmail.com>
2025-05-17 17:01:42 +0900

    TagReadService 테스트 코드 추가

---
+++ src/test/kotlin/com/erving/richstep/tag/service/TagReadServiceTest.kt
@@ -0,0 +1,42 @@
+package com.erving.richstep.tag.service
+
+import com.erving.richstep.mock.FakeTagRepository
+import com.erving.richstep.tag.domain.Tag
+import com.erving.richstep.user.domain.User
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class TagReadServiceTest {
+
+    private lateinit var tagReadService: TagReadServiceImpl
+
+    @BeforeEach
+    fun init() {
+        val fakeTagRepository = FakeTagRepository()
+        this.tagReadService = TagReadServiceImpl(fakeTagRepository)
+
+        val user = User(id = 1L, email = "test@user.com")
+
+        fakeTagRepository.save(Tag(user = user, name = "식비"))
+        fakeTagRepository.save(Tag(user = user, name = "교통"))
+        fakeTagRepository.save(Tag(user = user, name = "관리비"))
+        fakeTagRepository.save(Tag(user = user, name = "대출이자"))
+    }
+
+    @Test
+    fun user와_tagName이_일치하는_모든_Tag를_조회할_수_있다() {
+        // given
+        val user = User(id = 1L, email = "test@user.com")
+        val tagNames = listOf("교통", "관리비", "식비")
+
+        // when
+        val foundTags = this.tagReadService.findAllByUserAndNames(user, tagNames)
+
+        // then
+        assertThat(tagNames).hasSize(3)
+
+        val foundTagNames = foundTags.map { it.name }
+        assertThat(foundTagNames).containsExactlyInAnyOrder("교통", "관리비", "식비")
+    }
+}
